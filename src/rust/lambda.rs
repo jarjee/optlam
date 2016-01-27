46b9db55740be99402161dcb57f4a482f9f00f70
Nathan Smyth <nathan.andrew.smyth@gmail.com>
2016-01-27 21:52:41 +1100

    Correcting compilation errors in lambda.rs
    Since I hadn't properly configured cargo before, I had only fixed the
    syntactic errors, which meant that all the semantic errors had yet to be
    fixed.
    
    I've boxed my values, which means that this is currently dependent on
    the heap.

--- src/rust/lambda.rs
+++ src/rust/lambda.rs
@@ -1,30 +1,23 @@
 //Would be better if the terms could be typechecked (to make strongly normalising).
 //Currently working out the minimum required to enforce terms of only elementary-affine-logic
 //typeable terms.
+
+#[derive(Clone)]
 enum Term {
-    Lam(Term),
-    App(Term, Term),
-    Var(u32),
+    Lam(Box<Term>),
+    App(Box<Term>, Box<Term>),
+    Var(i32),
 }
 
 fn reduce (term: Term) -> Term {
-    match term {
-        Term::Var(x) => Term::Var(x),
-        Term::Lam(t) => Term::Lam(reduce(t)),
-        Term::App(l, r) => {
-            let left  = reduce(l);
-            let right = reduce(r);
-            match left {
-                Term::Lam(t) => reduce(substitute(right, true, 0, -1, left)),
-                _ => Term::App(left, right),
-            }
-        }
-    }
     fn substitute(value: Term, subs: bool, depth: i32, wrap: i32, term: Term) -> Term {
         match term {
-            Term::App(l,r) => Term::App(substitute(value, subs, depth, wrap, l)
-                                      ,substitute(value, subs, depth, wrap, r)),
-            Term::Lam(t) => Term::Lam(substitute(value, subs, depth+1, wrap, t)),
+            Term::App(l,r) => {
+                let v = value.clone();
+                Term::App(Box::new(substitute(value, subs, depth, wrap, *l))
+                , Box::new(substitute(v, subs, depth, wrap, *r)))
+            },
+            Term::Lam(t) => Term::Lam(Box::new(substitute(value, subs, depth+1, wrap, *t))),
             Term::Var(x) => if subs && x == depth {
                 substitute(Term::Var(0), false, -1, depth, value)
             } else {
@@ -33,13 +26,25 @@ fn reduce (term: Term) -> Term {
             }
         }
     }
+    match term {
+        Term::Var(x) => Term::Var(x),
+        Term::Lam(t) => Term::Lam(Box::new(reduce(*t))),
+        Term::App(l, r) => {
+            let left  = reduce(*l);
+            let right = reduce(*r);
+            match left {
+                Term::Lam(_) => reduce(substitute(right, true, 0, -1, left)),
+                _ => Term::App(Box::new(left), Box::new(right)),
+            }
+        },
+    }
 }
 
-fn fold<T>(var: Fn(u32) -> T, lam: Fn(T) -> T, app: Fn(T, T) -> T, term: Term) -> T{
+fn fold<T>(var: &Fn(i32) -> T, lam: &Fn(T) -> T, app: &Fn(T, T) -> T, term: Term) -> T{
     match term {
         Term::Var(x) => var(x),
-        Term::Lam(t) => lam(fold(var, lam, app, t)),
-        Term::App(l, r) => app(fold(var, lam, app, l), fold(var, lam, app, r)),
+        Term::Lam(t) => lam(fold(var, lam, app, *t)),
+        Term::App(l, r) => app(fold(var, lam, app, *l), fold(var, lam, app, *r)),
     }
 }
 
@@ -47,11 +52,11 @@ fn nat(num: u32) -> Term {
     fn go(x: u32) -> Term {
         match x {
             0 => Term::Var(0),
-            x => Term::App(Term::Var(1), go(x-1))
+            x => Term::App(Box::new(Term::Var(1)), Box::new(go(x-1)))
         }
     }
 
-    Term::Lam(Term::Lam(go(num)));
+    Term::Lam(Box::new(Term::Lam(Box::new(go(num)))))
 }
 
 fn nat_(term: Term) -> u32 {
@@ -59,5 +64,5 @@ fn nat_(term: Term) -> u32 {
 }
 
 fn size(term: Term) -> u32 {
-    fold(|x: u32| 1, |t: Term| term, |l: Term, r: Term| l + r)
+    fold(&|x: i32| 1, &|t: u32| t, &|l: u32, r: u32| l + r, term)
 }
